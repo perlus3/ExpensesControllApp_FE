199895ab91ee9b8c89bc0fbba9e1739900de5d60
Kamil <perlegus@gmail.com>
2023-06-20 20:04:41 +0200

    trying to develop better layout of DetailsView.tsx

--- src/components/views/DetailsView.tsx
+++ src/components/views/DetailsView.tsx
@@ -14,7 +14,11 @@ export const DetailsView = () => {
   const userContext = useContext(AuthContext);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | undefined>(undefined);
-  const [render, setRender] = useState(0);
+  const [showOperations, setShowOperations] = useState(false);
+  const [cashReport, setCashReport] = useState({
+    income: 0,
+    expenses: 0,
+  });
 
   const [categories, setCategories] = useState<CategoryEntity[]>([]);
   const [filteredOperations, setFilteredOperations] = useState<
@@ -28,10 +32,33 @@ export const DetailsView = () => {
     },
   ]);
 
-  const [selectedYear, setSelectedYear] = useState('');
-  const [selectedMonth, setSelectedMonth] = useState('');
+  const [selectedYear, setSelectedYear] = useState<string>('');
+  const [selectedMonth, setSelectedMonth] = useState<string>('');
   const [selectedCategoryId, setSelectedCategoryId] = useState('');
-  const isMonthsDisabled = selectedYear === '';
+  // const isMonthsDisabled = selectedYear === '';
+
+  const [filteredPeriod, setFilteredPeriod] = useState({
+    year: selectedYear,
+    month: selectedMonth,
+  });
+
+  const today = new Date().toLocaleDateString('pl-PL', {
+    year: 'numeric',
+    month: 'long',
+  });
+
+  const newDate = (date: Date) => {
+    return new Date(date).toLocaleDateString('pl-PL', {
+      year: 'numeric',
+      month: 'long',
+    });
+  };
+  //
+  // const data = selectedYear + selectedMonth;
+  // const year = data.substring(0, 4);
+  // const month = data.substring(4);
+  // const dateObject = new Date(`${year}-${month}-01`);
+  // const peroidDate = newDate(dateObject);
 
   const months: Month[] = [
     { name: 'Styczeń', value: 1 },
@@ -56,55 +83,84 @@ export const DetailsView = () => {
     years.push(i);
   }
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   try {
+  //     (async () => {
+  //       const res = await fetch(`${apiUrl}/categories`, {
+  //         method: 'GET',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           Authorization: `Bearer ${userContext?.token}`,
+  //         },
+  //       });
+  //       const data = await res.json();
+  //       if (!data.id) {
+  //         setError(data.message);
+  //       }
+  //       setCategories(data);
+  //     })();
+  //   } catch (error: any) {
+  //     setError(error.message);
+  //   }
+  // }, []);
+  // const checkDetails = async (e: SyntheticEvent) => {
+  //   e.preventDefault();
+  //   setLoading(true);
+  //
+  //   try {
+  //     const res = await fetch(
+  //       `${apiUrl}/categories/all/${selectedCategoryId}?year=${selectedYear}&month=${selectedMonth}`,
+  //       {
+  //         method: 'GET',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           Authorization: `Bearer ${userContext?.token}`,
+  //         },
+  //       },
+  //     );
+  //     const data = await res.json();
+  //
+  //     if (data) {
+  //       setFilteredOperations(data);
+  //       setShowOperations(true);
+  //     }
+  //     if (!data) {
+  //       setError(data.message);
+  //     }
+  //   } catch (e: any) {
+  //     setError('Aby kontynuować musisz wybrać kategorie operacji!');
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  const changeFilterPeriod = (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    // useEffect(() => {
     try {
       (async () => {
-        const res = await fetch(`${apiUrl}/categories`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${userContext?.token}`,
+        const res = await fetch(
+          `${apiUrl}/operations/total/report?year=${selectedYear}&month=${selectedMonth}`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${userContext?.token}`,
+            },
           },
-        });
+        );
         const data = await res.json();
         if (!data.id) {
           setError(data.message);
         }
-        setCategories(data);
+        console.log(data);
+        setCashReport(data);
       })();
     } catch (error: any) {
       setError(error.message);
     }
-  }, []);
-  const checkDetails = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    setLoading(true);
-
-    try {
-      const res = await fetch(
-        `${apiUrl}/categories/all/${selectedCategoryId}?year=${selectedYear}&month=${selectedMonth}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${userContext?.token}`,
-          },
-        },
-      );
-      const data = await res.json();
-
-      if (data) {
-        setFilteredOperations(data);
-        setRender(+1);
-      }
-      if (!data) {
-        setError(data.message);
-      }
-    } catch (e: any) {
-      setError('Aby kontynuować musisz wybrać kategorie operacji!');
-    } finally {
-      setLoading(false);
-    }
+    // }, []);
   };
 
   const handleCategoryIdChange = (e: any) => {
@@ -132,101 +188,151 @@ export const DetailsView = () => {
     return <ErrorHandler message={error} />;
   }
 
-  const newDate = (date: Date) => {
-    return new Date(date).toLocaleDateString('pl-PL', {
-      year: 'numeric',
-      month: 'numeric',
-      day: 'numeric',
-    });
-  };
-
   return (
-    <div className="col-6">
-      <div className="details-view col">
-        <form className="form" onSubmit={checkDetails}>
-          <h1>Wybierz kategorie</h1>
-          <select
-            className="form-select w-75"
-            name="categoryId"
-            value={selectedCategoryId}
-            onChange={handleCategoryIdChange}
-          >
-            <option value="">--Wybierz kategorie--</option>
-            {categories.map((category) => (
-              <option key={category.id} value={category.id}>
-                {category.name}
-              </option>
-            ))}
-          </select>
-          <h4>Wybierz rok:</h4>
-          <select
-            className="form-select w-75"
-            name="year"
-            value={selectedYear}
-            onChange={handleYearChange}
-          >
-            <option value="">--Wybierz--</option>
-            {years.map((year) => (
-              <option key={year} value={year}>
-                {year}
-              </option>
-            ))}
-          </select>
-          <h4>Wybierz miesiąc:</h4>
-          <select
-            className="form-select w-75"
-            name="month"
-            value={selectedMonth}
-            disabled={isMonthsDisabled}
-            onChange={handleMonthChange}
-          >
-            <option value="">--Wybierz--</option>
-            {months.map((month) => (
-              <option key={month.name} value={month.value}>
-                {month.name}
-              </option>
-            ))}
-          </select>
-          <button className="btn btn-primary w-50">Wybierz</button>
-          <GoBackButton />
-        </form>
-      </div>
-      {render ? (
-        <div className="details-list d-flex col mt-5 border">
-          <div className="col">
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th scope="col">Nazwa</th>
-                  <th scope="col">Wartość</th>
-                  <th scope="col">Data</th>
-                </tr>
-              </thead>
-              {filteredOperations.map((el) => (
-                <tbody key={el.id}>
-                  <tr>
-                    <td>{el.name}</td>
-                    <td>{el.value}</td>
-                    <td>{newDate(el.createdAt)}</td>
-                  </tr>
-                </tbody>
-              ))}
-            </table>
-          </div>
-          <div className="col">
-            <p className="text-center">
-              Suma wybranych operacji wyniosła:{' '}
-              <strong>
-                {filteredOperations.reduce(
-                  (sum, el) => sum + Number(el.value),
-                  0,
-                )}
-              </strong>
-            </p>
-          </div>
+    <div className="col-6 d-flex flex-column justify-content-center border border-white">
+      <div className="row">
+        <div className="col d-flex justify-content-center">
+          {/*<p className="text-white">*/}
+          {/*  {selectedMonth && selectedYear ? peroidDate : today}*/}
+          {/*</p>*/}
         </div>
-      ) : null}
-      <PieChart />
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <form onSubmit={changeFilterPeriod}>
+            <div className="d-flex">
+              <div className="me-3">
+                <p className="text-white">Wybierz rok:</p>
+                <select
+                  name="year"
+                  value={selectedYear}
+                  onChange={handleYearChange}
+                >
+                  <option value="">--Wybierz--</option>
+                  {years.map((year) => (
+                    <option key={year} value={year}>
+                      {year}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="me-3">
+                <p className="text-white">Wybierz miesiąc:</p>
+                <select
+                  name="month"
+                  value={selectedMonth}
+                  onChange={handleMonthChange}
+                >
+                  <option value="">--Wybierz--</option>
+                  {months.map((month) => (
+                    <option key={month.name} value={month.value}>
+                      {month.name}
+                    </option>
+                  ))}
+                </select>
+                <button className="btn btn-primary">Wybierz</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="row text-white">
+        <div className="col">
+          <p>Przychód całkowity</p>
+          <p>{cashReport.income}</p>
+        </div>
+      </div>
+      <div className="row text-white">
+        <div className="col">
+          <p>Wydatki</p>
+          <p>{cashReport.expenses}</p>
+        </div>
+      </div>
     </div>
+    // <div className="col-6 d-flex justify-content-center border border-white">
+    //   <form className="" onSubmit={checkDetails}>
+    //     <h3>Analizuj wydatki</h3>
+    //     <select
+    //       // className="form-select"
+    //       name="categoryId"
+    //       value={selectedCategoryId}
+    //       onChange={handleCategoryIdChange}
+    //     >
+    //       <option value="">--Wybierz kategorie--</option>
+    //       {categories.map((category) => (
+    //         <option key={category.id} value={category.id}>
+    //           {category.name}
+    //         </option>
+    //       ))}
+    //     </select>
+    //     <h4>Wybierz rok:</h4>
+    //     <select
+    //       // className="form-select"
+    //       name="year"
+    //       value={selectedYear}
+    //       onChange={handleYearChange}
+    //     >
+    //       <option value="">--Wybierz--</option>
+    //       {years.map((year) => (
+    //         <option key={year} value={year}>
+    //           {year}
+    //         </option>
+    //       ))}
+    //     </select>
+    //     <h4>Wybierz miesiąc:</h4>
+    //     <select
+    //       // className="form-select"
+    //       name="month"
+    //       value={selectedMonth}
+    //       disabled={isMonthsDisabled}
+    //       onChange={handleMonthChange}
+    //     >
+    //       <option value="">--Wybierz--</option>
+    //       {months.map((month) => (
+    //         <option key={month.name} value={month.value}>
+    //           {month.name}
+    //         </option>
+    //       ))}
+    //     </select>
+    //     <button className="btn btn-primary w-50">Wybierz</button>
+    //     <GoBackButton />
+    //   </form>
+    //   {showOperations ? (
+    //     <div className="details-list d-flex col mt-5 border">
+    //       <div className="col">
+    //         <table className="table table-striped text-white">
+    //           <thead>
+    //             <tr>
+    //               <th scope="col">Nazwa</th>
+    //               <th scope="col">Wartość</th>
+    //               <th scope="col">Data</th>
+    //             </tr>
+    //           </thead>
+    //           {filteredOperations.map((el) => (
+    //             <tbody key={el.id}>
+    //               <tr>
+    //                 <td>{el.name}</td>
+    //                 <td>{el.value}</td>
+    //                 <td>{newDate(el.createdAt)}</td>
+    //               </tr>
+    //             </tbody>
+    //           ))}
+    //         </table>
+    //       </div>
+    //       <div className="col">
+    //         <p className="text-center text">
+    //           Suma wybranych operacji wyniosła:{' '}
+    //           <strong>
+    //             {filteredOperations.reduce(
+    //               (sum, el) => sum + Number(el.value),
+    //               0,
+    //             )}
+    //           </strong>
+    //         </p>
+    //       </div>
+    //     </div>
+    //   ) : null}
+    //   {/*<PieChart />*/}
+    // </div>
   );
 };
