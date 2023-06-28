7cb1c4710701c2c6f56ce09c74794a51768080a4
Kamil <perlegus@gmail.com>
2023-06-28 22:57:26 +0200

    small changes in Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,37 +4,30 @@ import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg background-color">
-      <div className="container">
-        <Link to="#" className="navbar-brand text-white">
+    <nav className="navbar navbar-expand-lg background-color border-bottom border-dark">
+      <div className="container d-flex justify-content-between align-items-center">
+        <Link to="/user" className="navbar-brand">
           ExpenseApp
         </Link>
         <button
           className="navbar-toggler"
+          type="button"
           data-bs-toggle="collapse"
           data-bs-target="#nav"
           aria-controls="nav"
-          aria-label="Rozwiń nawigacje"
+          aria-label="Toggle navigation"
         >
-          <div className="navbar-toggler-icon text-white"></div>
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="nav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link
-                to="/new-account"
-                className="nav-link text-white"
-                aria-current="page"
-              >
+              <Link to="/new-account" className="nav-link" aria-current="page">
                 Dodaj Konto
               </Link>
             </li>
             <li className="nav-item">
-              <Link
-                to="/log-out"
-                className="nav-link text-white"
-                aria-current="page"
-              >
+              <Link to="/log-out" className="nav-link" aria-current="page">
                 Wyloguj
               </Link>
             </li>
