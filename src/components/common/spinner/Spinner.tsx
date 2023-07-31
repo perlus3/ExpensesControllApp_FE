63e82102a770f4a7698978218577d0f36e5e1fa1
Kamil <perlegus@gmail.com>
2023-07-31 21:14:44 +0200

    added spinner for loading

---
+++ src/components/common/spinner/Spinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Spinner.css';
+
+export const Spinner = () => {
+  return (
+    <div className="container" style={{ padding: '200px' }}>
+      <div className="spinner" />
+    </div>
+  );
+};
